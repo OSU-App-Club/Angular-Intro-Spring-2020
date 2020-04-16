84ef7929801a47286bd49ffd4d727f527f525db8
Jonathan Alexander <me@jmalexan.com>
2020-04-15 18:03:57 -0700

    Work from 4/8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DayForecastComponent } from './day-forecast/day-forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DayForecastComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/day-forecast/day-forecast.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-day-forecast',
+  templateUrl: './day-forecast.component.html',
+  styleUrls: ['./day-forecast.component.scss']
+})
+export class DayForecastComponent implements OnInit {
+
+  @Input() forecast: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.forecast);
+  }
+
+}
